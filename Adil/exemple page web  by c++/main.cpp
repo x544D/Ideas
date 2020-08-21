66a3a3bb98d1e749404acd644dd24a297730b76e
AMRANI SAAD <62079074+x544D@users.noreply.github.com>
2020-08-21 00:02:27 -0700

    Update main.cpp

--- Adil/exemple page web  by c++/main.cpp
+++ Adil/exemple page web  by c++/main.cpp
@@ -5,7 +5,15 @@
 
 using namespace std;
 
-
+/*
+ *  Bon to use this syntax for a full WebPageProject the only condition is : the file extension must be .wals (web als)
+ *    |__ then the user wont need to load @webpage , instead he is allowed to use all functions of web module directly,
+ *        But not allowed to use basic als Syntax
+ *
+ *  if the user wants to use this syntax inside an .als project he needs to from @base load @webpage
+ *    |__ then he can do use directly $head() ... $body()... etc
+ *  
+ */
 
 int main() {
 
